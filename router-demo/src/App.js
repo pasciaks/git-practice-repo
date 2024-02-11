1afd5837a30942334db700a05c02fa8519084012
Sheldon Pasciak <sheldon@pasciak.com>
2024-02-11 09:51:13 -0700

    Image assets and bundling etc

--- router-demo/src/App.js
+++ router-demo/src/App.js
@@ -1,13 +1,32 @@
 
 import './App.css';
-import { Routes, Route, NavLink, Link } from 'react-router-dom';
+import { Routes, Route, /*NavLink,*/ Link } from 'react-router-dom';
 import Homepage from './Homepage';
 import AboutMe from './AboutMe';
+import cat from "./assets/cat.png";
+import dog from "./assets/images/dog.png";
 
 function App() {
+  const test = true;
+  let randomImageUrl = 'https://picsum.photos/536/354';
+  let date = new Date();
   return (
     <div className="App">
       <h1>Router Demo</h1>
+      <img src={cat} alt="cat" />
+      <img src={require("./assets/cat.png")} alt="cat" />
+      <div> {
+        test && `Hello`} World </div>
+      {date.toDateString()}
+      <hr></hr>
+      <img src={dog} alt="dog" />
+      <img src={require("./assets/images/dog.png")} alt="dog" />
+      <img src={randomImageUrl} alt="dog" />
+      <hr></hr>
+      <hr></hr>
+      {/* NOTE- because of bundling you can't link directly to src folder, only public folder */}
+      <img src="./assets/images/dog.png" alt="dog" />
+      <img src="logo512.png" alt="dog" />
       <nav>
         <ul>
           <li>
